cd253ba3bda6ee47c193e4f4100a922a23b95a54
israr <israr@192.168.86.23>
2020-02-02 16:46:50 -0500

    shayan_0521

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,56 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+//	This is a jave comment
+	
+//	this is another 
+	
+//	this
+//	is
+//	a
+//	
+//	comment
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =1;
+		int y =2;
+		int j =3;
+		x =5;
+		
+//  	double datatype
+		double a =1.5;
+		double d =2.5;
+		
+//		character datatype
+		char z= 'b';
+		
+//		String is a non premitive data type. String is class
+		
+		String name = "Shomir";
+		
+//		Boolean Datatype
+		
+		boolean b =true;
+		boolean c =false;
+		
+		System.out.println("My name is "
+				+ "");
+
+	}
+	
+	
+	public static void Addition() {
+		
+//		TODO Auto-generated stub
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
