5ba851d3782992f88b069b40168b005d487b9760
israr <israr@LAPTOP-3CV495NQ.fios-router.home>
2020-02-04 15:37:09 -0500

    My First Push

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/pushtry.java
@@ -0,0 +1,33 @@
+package com.DataTypePackage;
+
+public class pushtry {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(2+2);
+     addition();
+    SUBTRACT();
+    DIVISION();
+		
+		
+		
+	}
+	
+	public static void addition() {
+		
+		System.out.println(5+5);
+	}
+public static void SUBTRACT() {
+		
+		System.out.println(10-5);
+	}
+public static void DIVISION() {
+	
+	System.out.println(5/5);
+}
+
+
+}
